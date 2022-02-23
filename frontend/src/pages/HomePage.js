1c97e80c7ff124cc592b39521d6fa99534fd9070
jamescaggeles <jamescaggeles@gmail.com>
2022-02-23 13:17:17 -0500

    home page added

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+
+import TeamTile from '../components/TeamTile';
+import YearTile from '../components/YearTile';
+import logo from '../assets/images/versus-logo.png';
+
+import './HomePage.scss';
+
+const HomePage = () => {
+  const [teams, setTeams] = useState();
+  const [isFetched, setIsFetched] = useState(false);
+
+  let years = [];
+  const startYear = process.env.REACT_APP_DATA_START_YEAR;
+  const endYear = process.env.REACT_APP_DATA_END_YEAR;
+
+  for (let i = startYear; i <= endYear; i++) {
+    years.push(i);
+  }
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const response = await fetch(`http://localhost:8080/teams`);
+      const data = await response.json();
+      setTeams(data);
+      setIsFetched(true);
+    };
+
+    fetchTeams();
+  }, []);
+
+  return (
+    <div className='HomePage'>
+      <div className='home-header'>
+        <img className='app-logo' src={logo} alt='Versus Logo' />
+        <h1 className='app-tag-line'>Select Team</h1>
+      </div>
+      <div className='team-grid'>
+        {isFetched &&
+          teams.map((team) => {
+            return <TeamTile key={Math.random()} teamName={team} />;
+          })}
+      </div>
+      {/* <div className='year-grid'>
+        {isFetched &&
+          years.map((year) => {
+            return <YearTile key={Math.random()} year={year} />;
+          })}
+      </div> */}
+    </div>
+  );
+};
+
+export default HomePage;
