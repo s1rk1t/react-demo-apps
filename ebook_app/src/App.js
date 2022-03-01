b8a47542659d3b7c0f3f40c20b472b049d05a871
jamescaggeles <jamescaggeles@gmail.com>
2022-02-28 19:37:48 -0500

    improved modal logic and added styling

--- ebook_app/src/App.js
+++ ebook_app/src/App.js
@@ -1,11 +1,23 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [isCartShown, setIsCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setIsCartShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {isCartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- ebook_app/src/components/Layout/Header.js
+++ ebook_app/src/components/Layout/Header.js
@@ -11,7 +11,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>Bookmark</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={food} alt='food'></img>
