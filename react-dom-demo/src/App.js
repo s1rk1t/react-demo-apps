f84d812ee398a89b041bab80f2bfc2061bede956
jamescaggeles <jamescaggeles@gmail.com>
2022-03-08 14:27:06 -0500

    added react-dom-demo app

---
+++ react-dom-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Hi there!</h1>
+      <p>This is new!</p>
+    </div>
+  );
+}
+
+export default App;
