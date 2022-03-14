dd5eb516533b25fd91a0ff0e9c4596d120767678
jamescaggeles <jamescaggeles@gmail.com>
2022-03-14 15:13:10 -0400

    refactored custom hook and updated versus app request logic

--- custom-hooks-http/src/App.js
+++ custom-hooks-http/src/App.js
@@ -1,40 +1,11 @@
 import React, { useEffect, useState } from 'react';
-
+import useHttp from './hooks/use-http';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-aggeles-default-rtdb.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
+  useHttp();
   useEffect(() => {
     fetchTasks();
   }, []);
